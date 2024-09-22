a0149efadd4f1f8be115a338c9262558dcb0e1f2
csathishkumar <csathishkumar2002@gmail.com>
2024-09-22 18:13:47 +0530

    Created HTML files with Bootstrap 5

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, "shop/index.html")
+
+def register(request):
+    return render(request, "shop/register.html")
