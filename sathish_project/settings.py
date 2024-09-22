2b0b2d83b8bdbc0fdf1f240b8ff3adab0a83d53e
csathishkumar <csathishkumar2002@gmail.com>
2024-09-22 20:43:24 +0530

    created a static folder to store images and connected with setting.py

--- sathish_project/settings.py
+++ sathish_project/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR/'statics'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
