d44dcdd50d4a8e997293aaa54fd8263fc758d48b
csathishkumar <csathishkumar2002@gmail.com>
2024-09-23 15:22:21 +0530

    created the collection page with bootstrap

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('',views.home,name="home"),
     path('home',views.home,name="home"),
-    path('register',views.register,name="register")
+    path('register',views.register,name="register"),
+    path('collections',views.collections,name="collections")
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
 def home(request):
@@ -6,3 +8,7 @@ def home(request):
 
 def register(request):
     return render(request, "shop/register.html")
+
+def collections(request):
+    category = Category.objects.filter(status = 0)
+    return render(request, "shop/collections.html", {"category":category})
