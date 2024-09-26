fedd5b619acaba267f70c6c2930c21bfbbf7ab79
csathishkumar <csathishkumar2002@gmail.com>
2024-09-26 18:03:46 +0530

    Login Login working perfectly

--- shop/views.py
+++ shop/views.py
@@ -3,15 +3,31 @@ from .models import *
 from shop.form import *
 from django.contrib import messages
 from django.shortcuts import redirect
-from django.http import *
-
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def home(request):
     products = Product.objects.filter(trending = 1)    
     return render(request, "shop/index.html", {"products":products})
 
+def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,"Logged out Successfully")
+    return redirect("/")
+ 
 def login_page(request):
+    if request.method=='POST':
+        name = request.POST.get('username')
+        pwd = request.POST.get('password')
+        user = authenticate(request,username=name,password=pwd)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Logged in Successfully")
+            return redirect("/")
+        else:
+            messages.error(request,"Invalid Username or Password")
+            return redirect("/login")
     return render(request, "shop/login.html")
 
 def register(request):
