86106be7c5b21c4358a8e23d6168d8620729189a
csathishkumar <csathishkumar2002@gmail.com>
2024-09-28 05:52:50 +0530

    Pushing for small doubt

--- shop/views.py
+++ shop/views.py
@@ -1,21 +1,46 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import *
 from shop.form import *
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate,login,logout
+import json
 
 # Create your views here.
 def home(request):
     products = Product.objects.filter(trending = 1)    
     return render(request, "shop/index.html", {"products":products})
 
+
+def add_to_cart(request):
+    if request.headers.get('x-requested-with')=='XMLHttpRequest':
+        if request.user.is_authenticated:
+            data = json.load(request)
+            product_qty = data['product_qty']
+            product_id = data['pid']
+            # print(request.user.id)
+            product_status = Product.objects.get(id=product_id)
+            if product_status:
+                if Cart.objects.filter(user=request.user.id,product_id=product_id):
+                    return JsonResponse({'status': 'Product Already in Cart'},status=200)                    
+                else:
+                    if product_status.quantity>=product_qty:
+                        Cart.objects.create(user=request.user,product_id=product_id,product_qty=product_qty)
+                        return JsonResponse({'status': 'Product Added in Cart'},status=200)                    
+                    else:
+                        return JsonResponse({'status': 'Product Not Available'},status=200)                    
+        else:
+            return JsonResponse({'status': 'Login to Add Cart'},status=200)
+    else:
+        return JsonResponse({'status': 'Invalid Access'},status=200)
+
 def logout_page(request):
     if request.user.is_authenticated:
         logout(request)
         messages.success(request,"Logged out Successfully")
     return redirect("/")
- 
+
 def login_page(request):
     if request.user.is_authenticated:
         return redirect("/")
@@ -32,7 +57,7 @@ def login_page(request):
                 messages.error(request,"Invalid Username or Password")
                 return redirect("/login")
         return render(request, "shop/login.html")
-    
+
 def register(request):
     form = CustomerUserForm()
     if request.method=="POST":
@@ -67,4 +92,3 @@ def product_details(request,cname,pname):
     else:
         messages.error(request,"No Such Product Found")
         return redirect('collections')
- 
